d99da07c528d208d5a1916b048d97e797d15d23e
dpebert7 <dpebert7@gmail.com>
2017-03-11 13:02:11 -0600

    initial commit

---
+++ email.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Save this file to desktop. Uncomment the email.
+
+
+# Crontab notes:
+#
+# In shell, enter crontab -e to edit crontab file
+#
+# * * * * * ~/Desktop/email.sh     <- sends email every minute
+#
+# 0 11 * * 2 ~/Desktop/email.sh    <- sends on Tuesday at 11 AM
+#
+# 0 11 * * 4 ~/Desktop/email.sh    <- sends on Thursday at 11 AM 
+#
+# 0 11 * * 2,4 ~/Desktop/email.sh  <- sends on Tuesdays and Thursdays at 11 AM
+#
+# email@tarleton.edu
+
+
+#time=$(date)
+
+#echo "Dr Winton,
+
+#Last Tuesday we presented problems from chapter 7.
+
+#See you in class,
+
+# - David" | mail -s "Topology Reminder" email@tarleton.edu
+
+
